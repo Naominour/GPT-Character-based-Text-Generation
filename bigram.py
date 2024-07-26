5a374d5dd3084ffcb646dbf69b178d7e27746b11
Naomi <naeimehnour@gmail.com>
2024-07-26 22:28:38 +0100

    GPT v-01 completed

--- bigram.py
+++ bigram.py
@@ -3,14 +3,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32 # number of independent sequences we will process in parallel
-block_size = 8 # number of maximum context lenght for predicctions
+batch_size = 64 # number of independent sequences we will process in parallel
+block_size = 256 # number of maximum context lenght for predicctions
 max_iters = 5000
 eval_interval = 500
-learning_rate = 1e-3
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_mbed = 32
+n_mbed = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
 #--------------
 
 torch.manual_seed(1337)
@@ -66,6 +69,7 @@ class Head(nn.Module):
        self.query = nn.Linear(n_mbed, head_size, bias=False)
        self.value = nn.Linear(n_mbed, head_size, bias=False)
        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+       self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         B, T, C = x.shape
@@ -75,10 +79,56 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2,-1) * C**-0.5
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
         wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
 
         v = self.value(x)
         out = wei @ v
         return out
+
+class MultiHeadAttention(nn.Module):
+   
+   def __init__(self, num_heads, head_size):
+      super().__init__()
+      self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+      self.proj = nn.Linear(n_mbed, n_mbed)
+      self.dropout = nn.Dropout(dropout)
+
+   def forward(self, x):
+      out = torch.cat([h(x) for x in self.heads], dim=-1)
+      out = self.proj(out)
+      return out 
+    
+
+class FeedForward(nn.Module):
+   def __init__(self, n_embd):
+    super().__init__()
+    self.net = nn.Sequential(
+        nn.Linear(n_mbed, 4 * n_mbed),
+        nn.ReLU(),
+        nn.Linear(4 * n_mbed, n_mbed),
+        nn.Dropout(dropout),  # apply dropout to the output of the feed-forward network
+   )
+   def forward(self, x):
+      return self.net(x)
+
+
+
+class Block(nn.Module):
+   
+   def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+   def forward(self, x):
+        x = self.sa(self.ln1(x))
+        x = self.ffwd(self.ln2(x)) 
+        return x
+      
+
 
 
 class BigramLanguageModel(nn.Module):
@@ -87,7 +137,9 @@ class BigramLanguageModel(nn.Module):
     super().__init__()
     self.token_embedding_table = nn.Embedding(vocab_size, n_mbed)
     self.position_embedding_table = nn.Embedding(block_size, n_mbed)
-    self.sa_head = Head(n_mbed)
+    self.blocks = nn.Sequential(*[Block(n_mbed, n_head=n_head) for _ in range(n_layer)])
+    self.sa_heads = MultiHeadAttention(4, n_mbed//4)
+    self.ffwd = FeedForward(n_mbed)
     self.lm_head = nn.Linear(n_mbed, vocab_size)
 
   def forward(self, idx, targets=None):
@@ -95,7 +147,9 @@ class BigramLanguageModel(nn.Module):
     tok_emb = self.token_embedding_table(idx) # batch, time, channels
     pos_emb = self.position_embedding_table(torch.arange(T, device=device))
     x = tok_emb + pos_emb 
-    x = self.sa_head(x) # batch, time, channels
+    x = self.sa_heads(x) # batch, time, channels
+    x = self.ffwd(x) # batch, time, channels
+    x = self.blocks(x)
     logits = self.lm_head(x) # batch, time, vocab_size
    
     if targets is None:
